82842ae11150a49510844845cc1b54ea1a7a5dd3
PatTheHyruler <pmcroofplays@gmail.com>
2022-09-24 20:03:47 +0300

    Almost fully implement ConsoleMenuFactory

--- Checkers/ConsoleClient/Program.cs
+++ Checkers/ConsoleClient/Program.cs
@@ -7,10 +7,10 @@ Console.WriteLine("Hello, World!");
 var n = new MenuItem("New game", () => Console.WriteLine("NEWGAME"));
 var l = new MenuItem("Load game", () => Console.WriteLine("LOADGAME"));
 
-var a = new MenuItem("Aadsfasdf", () => Console.WriteLine("ASDHGSJHDGASJ"));
+var a = new MenuItem("Aadsfaasdfasdfasdfjhakdefcklasdkcfjasdhjfkjvsadhfjksadhfkjasdhfkjasdhfkjasdhfvkjsadfhsadkjdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddddddddddddddsdf", () => Console.WriteLine("ASDHGSJHDGASJ"));
 var b = new MenuItem("Brgagaf", () => Console.WriteLine("BDFGJDHFHJKA"));
 
-var extraMenuCreator = new ConsoleMenuFactory("Extra", n, l, a);
+var extraMenuCreator = new ConsoleMenuFactory("Extra", n, l, a, b, n, l, l, l, a, l, n, b, l, a);
 var extraItemCreator = new MenuItem("Extra", extraMenuCreator);
 var optionsMenuCreator = new ConsoleMenuFactory("Options", a, b, l, extraItemCreator);
 var optionsItemCreator = new MenuItem("Options", optionsMenuCreator);
--- Checkers/MenuSystem/ConsoleMenuFactory.cs
+++ Checkers/MenuSystem/ConsoleMenuFactory.cs
@@ -2,6 +2,9 @@
 
 public class ConsoleMenuFactory : MenuFactory
 {
+    public const int MaxMenuHeight = 10;
+    public const int LineSeparatorWidth = 32;
+
     private ConsoleMenuFactory(string title, Func<Menu, EMenuFunction> menuLoop, params MenuItem[] menuItems) : base(
         title, menuLoop, menuItems)
     {
@@ -11,33 +14,56 @@ public class ConsoleMenuFactory : MenuFactory
     {
     }
 
+    private static int WriteMenuHeader(Menu menu)
+    {
+        var writtenLines = 0;
+        var menuPath = menu.MenuPath;
+        if (menuPath.Length == 0)
+        {
+            menuPath = "MENU PATH NOT FOUND???";
+        }
+
+        OverWriteLine();
+        writtenLines++;
+        OverWriteLine(menuPath);
+        writtenLines++;
+        return writtenLines;
+    }
+
+    private static void OverWriteLine(string text = "", bool highlight = false)
+    {
+        var previousBackgroundColor = Console.BackgroundColor;
+        if (highlight) Console.BackgroundColor = ConsoleColor.White;
+        var processedText = text.Length > Console.WindowWidth ? text[..(Console.WindowWidth - 3)] + "..." : text;
+        Console.Write(processedText);
+        Console.BackgroundColor = previousBackgroundColor;
+        Console.Write(new string(' ', Console.WindowWidth - processedText.Length));
+    }
+
+    private static void OverWriteLine(char pattern, int amount = LineSeparatorWidth)
+    {
+        OverWriteLine(new string(pattern, amount));
+    }
+
     private static Func<Menu, EMenuFunction> _consoleMenuLoop = menu =>
     {
+        Console.CursorVisible = false; // TODO: Make visible again when done
+        var initialCursorTop = Console.CursorTop;
         do
         {
-            Console.WriteLine("\n");
-            var menuPath = menu.MenuPath;
-            if (menuPath.Length > 0) Console.WriteLine(menuPath);
-            Console.WriteLine("========================");
+            var headerHeight = WriteMenuHeader(menu);
+            var menuItemsHeight = MaxMenuHeight - headerHeight - 2;  // -2 for the surrounding separator lines
 
-            var menuItems = menu.MenuItems;
+            var page = menu.CursorPosition / menuItemsHeight;
+            var menuItemsStart = page * menuItemsHeight;
 
-            for (var i = 0; i < menuItems.Count; i++)
-            {
-                var menuItem = menuItems[i];
+            OverWriteLine(page == 0 ? '_' : '▲');
+            WriteMenuItems(menu, menuItemsStart, menuItemsStart + menuItemsHeight);
 
-                if (i == menu.CursorPosition)
-                {
-                    var previousBackgroundColor = Console.BackgroundColor;
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.WriteLine(menuItem);
-                    Console.BackgroundColor = previousBackgroundColor;
-                }
-                else
-                {
-                    Console.WriteLine(menuItem);
-                }
-            }
+            var maxPage = (menu.MenuItems.Count - 1) / menuItemsHeight;
+            OverWriteLine(page < maxPage ? '▼' : '_');
+
+            Console.SetCursorPosition(0, initialCursorTop);
 
             var pressedKey = Console.ReadKey(true).Key;
             switch (pressedKey)
@@ -49,7 +75,7 @@ public class ConsoleMenuFactory : MenuFactory
                     menu.DecrementCursorPosition();
                     break;
                 case ConsoleKey.Enter:
-                    var menuItem = menuItems[menu.CursorPosition];
+                    var menuItem = menu.MenuItems[menu.CursorPosition];
                     var menuFunction = menuItem.Run(menu);
                     switch (menuFunction)
                     {
@@ -64,6 +90,25 @@ public class ConsoleMenuFactory : MenuFactory
 
                     break;
             }
+            
+            Console.SetCursorPosition(0, initialCursorTop);
         } while (true);
     };
+
+    private static void WriteMenuItems(Menu menu, int start, int end)
+    {
+        var menuItems = menu.MenuItems;
+        for (var i = start; i < end; i++)
+        {
+            if (i < menuItems.Count)
+            {
+                var menuItem = menuItems[i];
+                OverWriteLine(menuItem.Text, i == menu.CursorPosition);
+            }
+            else
+            {
+                OverWriteLine();
+            }
+        }
+    }
 }
