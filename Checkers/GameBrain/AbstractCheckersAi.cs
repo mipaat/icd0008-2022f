95346b88b86a6ad757f4313091c7b5fbbb549477
mipaat <mikkel.paat@gmail.com>
2022-12-11 04:34:30 +0200

    Allow AI to attempt to end turn early

--- Checkers/GameBrain/AbstractCheckersAi.cs
+++ Checkers/GameBrain/AbstractCheckersAi.cs
@@ -6,11 +6,18 @@ public abstract class AbstractCheckersAi : ICheckersAi
 {
     public abstract EAiType AiType { get; }
 
-    protected abstract Move GetMove(CheckersBrain checkersBrain);
+    protected abstract Move? GetMove(CheckersBrain checkersBrain);
 
     public void Move(CheckersBrain checkersBrain)
     {
         var move = GetMove(checkersBrain);
-        checkersBrain.Move(move.FromX, move.FromY, move.ToX, move.ToY);
+        if (move == null)
+        {
+            checkersBrain.EndTurn();
+        }
+        else
+        {
+            checkersBrain.Move(move.FromX, move.FromY, move.ToX, move.ToY);
+        }
     }
 }
