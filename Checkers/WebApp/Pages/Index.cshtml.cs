61aef4ab98621fc1c56e2c4fd0382b7b8fa02a38
mipaat <mipaat@ttu.ee>
2022-12-30 17:32:11 +0200

    Customize index page

--- Checkers/WebApp/Pages/Index.cshtml.cs
+++ Checkers/WebApp/Pages/Index.cshtml.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    
+    [BindProperty(SupportsGet = true)] public string? Error { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
